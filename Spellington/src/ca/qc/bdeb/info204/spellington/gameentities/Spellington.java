1742c24f0897cb37f2710fb2cfafdf6553e2b73c
Celtis24 <Celtis@rocketmail.com>
2017-02-10 21:18:49 -0500

    préparation

--- Spellington/src/ca/qc/bdeb/info204/spellington/gameentities/Spellington.java
+++ Spellington/src/ca/qc/bdeb/info204/spellington/gameentities/Spellington.java
@@ -10,13 +10,21 @@ import org.newdawn.slick.SlickException;
  *
  * @author Fallen Angel
  */
-public class Spellington extends GameEntity {
+public class Spellington extends LivingEntity {
 
     private static Image IMG_SPELLINGTON;
-    private static final float SPELLINGTON_SPEED = 0.5f;
 
     public Spellington() throws SlickException {
-        IMG_SPELLINGTON = new Image("resources/images/spellington.jpg");
+        LifePointMax = 100;
+        LifePoint = LifePointMax;
+        
+        ResElectric = 0;
+        ResIce = 0;
+        ResFire = 0;
+        
+        MovementSpeed = 0.5f;
+        
+        IMG_SPELLINGTON = new Image("resources/images/spellington.png");
         this.setX(500);
         this.setY(500);
 
@@ -29,19 +37,19 @@ public class Spellington extends GameEntity {
      * length.
      */
     public void update(Input input, int delta) {
-        if (input.isKeyDown(Input.KEY_RIGHT) && input.isKeyDown(Input.KEY_LEFT)) {
-            //If both buttons are pressed, the player does not move on the x axis.
-        } else if (input.isKeyDown(Input.KEY_RIGHT)) {
-            this.setX(this.getX() + SPELLINGTON_SPEED * delta);
-        } else if (input.isKeyDown(Input.KEY_LEFT)) {
-            this.setX(this.getX() - SPELLINGTON_SPEED * delta);
+        if (input.isKeyDown(Input.KEY_RIGHT) && input.isKeyDown(Input.KEY_LEFT)) {// à changer devrais suivre la souris
+            
+        } else if (input.isKeyDown(Input.KEY_D)) {
+            this.setX(this.getX() + MovementSpeed * delta);
+        } else if (input.isKeyDown(Input.KEY_A)) {
+            this.setX(this.getX() - MovementSpeed * delta);
         }
-        if (input.isKeyDown(Input.KEY_UP) && input.isKeyDown(Input.KEY_DOWN)) {
-            //If both buttons are pressed, the player does not move on the y axis.
-        } else if (input.isKeyDown(Input.KEY_UP)) {
-            this.setY(this.getY() - SPELLINGTON_SPEED * delta);
-        } else if (input.isKeyDown(Input.KEY_DOWN)) {
-            this.setY(this.getY() + SPELLINGTON_SPEED * delta);
+        if (input.isKeyDown(Input.KEY_UP) && input.isKeyDown(Input.KEY_DOWN)) { // à changer pour pas voler
+            
+        } else if (input.isKeyDown(Input.KEY_W)) {
+            this.setY(this.getY() - MovementSpeed * delta);
+        } else if (input.isKeyDown(Input.KEY_S)) {
+            this.setY(this.getY() + MovementSpeed * delta);
         }
 
     }
